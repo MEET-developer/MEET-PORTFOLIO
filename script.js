3eaae6a75f15d470488123443036e1d0e8a45275
MEET-developer <vishwakarmameet569@gmail.com>
2026-07-27 19:11:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,23 @@
 ===================================================== */
 
 /* ==========================================
+            LOADER
+========================================== */
+
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+
+        loader.style.opacity = "0";
+        loader.style.visibility = "hidden";
+
+    }, 1800);
+
+});
+
+/* ==========================================
             CUSTOM CURSOR
 ========================================== */
 
@@ -539,6 +556,7 @@ if (menuBtn && navMenu) {
 
     });
 
+}
 
 /* ==========================================
             RESIZE FIX
